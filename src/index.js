b352b8d23812fa8cfdbc95fa545da426355e8476
Abhishek Sharma <5h1dabhi@gmail.com>
2019-10-22 01:18:19 +0530

    socket.io events

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
 const hbs = require('hbs')
+const socketio = require('socket.io')
 
 
 const app = express()
+const server = http.createServer(app)
+const io= socketio(server)
+
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
-    console.log(`Server is up onport ${port}`)
+let count = 0
+io.on('connection', (socket) => {
+    console.log('New Web socket connection.')
+
+    socket.emit('countUpdated', count)
+
+    socket.on('increment', () => {
+        count++
+        // socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+    })
+
+})
+
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
 })
 
